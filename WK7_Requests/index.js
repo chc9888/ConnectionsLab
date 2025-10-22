aeb74059e0a39daa21997256b52549d03f43132d
corc <chc9888@nyu.edu>
2025-10-22 15:47:50 -0600

    Week 7 assignment finished

---
+++ WK7_Requests/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+
+//install and load losdb module
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
+const app = express();
+
+//connect to the DB
+const defaultData = { coffeTrackerData: [] };
+const adapter = new JSONFile('db.json');
+const db = new Low(adapter, defaultData);
+
+app.use(express.json());
+
+let coffeTracker = [];
+
+//2. add a route on server, that is listening for a post request
+app.post('/noCups', (req, res) => {
+    console.log(req.body);
+    let currentDate = Date().toString;
+    let obj = {
+        date: currentDate,
+        coffee: req.body.number
+    }
+    // add value to the lowdb database
+    db.data.coffeTrackerData.push(obj);
+    db.write()
+        .then(() => {
+            res.json({ task: "success" });
+        })
+})
+
+app.use('/', express.static('public'));
+app.listen(5000, () => {
+    console.log('listening at localhost:5000');
+})
+
+//add route to get all coffee track information
+app.get('/getCups', (req, res) => {
+    
+    //fetch data from the lowdb database
+    db.read()
+    .then(() =>{
+        let obj = {data: db.data.coffeTrackerData}
+        res.json(obj);
+    })
+})
---
+++ WK7_Requests/public/app.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', () => {
+    document.getElementById('button-coffee').addEventListener('click', () => {
+        let noCups = document.getElementById('number-coffee').value;
+        console.log(noCups);
+
+        //create object
+        let obj = { "number": noCups }
+        //stringify object
+        let jsonData = JSON.stringify(obj);
+        //fetch to route noCups
+        fetch('/noCups', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            //send object
+            body: jsonData
+        })
+            .then(response => response.json())
+            .then(data => {console.log(data) })
+
+        //1. make a fetch request of type POST so that we can send the info to the server -> index.js
+
+    })
+
+    document.getElementById('get-tracker').addEventListener('click', ()=>{
+        //get info on all the coffees we've had so far
+        fetch('/getCups')
+        .then (resp=> resp.json())
+        .then(data =>{
+            document.getElementById('coffee-info').innerHTML = ''
+            console.log(data.data);
+            for(let i=0; i<data.data.length; i++){
+                let string = data.data[i].date + " : " + data.data[i].coffee;
+                let elt = document.createElement('p');
+                elt.innerHTML = string;
+                document.getElementById('coffee-info').appendChild(elt);
+            }
+        })
+    })
+})
